53dc33c19019fec9804113d2a79ca7d2b4cb9290
dani190107 <daniil190107@gmail.com>
2020-12-01 22:40:02 +0300

    .

--- main.py
+++ main.py
@@ -11,6 +11,32 @@ def display_field():
     print('\n', field[0], '\n', field[1], '\n', field[2])
 
 
+def get_col_arr(col):
+    return [field[0][col], field[1][col], field[2][col]]
+
+
+def choose_pos():
+    # rand_col_ = random.choice([0, 1, 2])
+    # rand_row_ = random.choice([0, 1, 2])
+    # while field[rand_row_][rand_col_].strip():
+    #     rand_col_ = random.choice([0, 1, 2])
+    #     rand_row_ = random.choice([0, 1, 2])
+    many_occurs_cols = [0]
+    many_occurs_rows = [0]
+    for i in range(len(field)):
+        if field[i].count('X') >= 2:
+            many_occurs_rows.append(i)
+    for i in range(len([get_col_arr(0), get_col_arr(1), get_col_arr(2)])):
+        if field[i].count('X') >= 2:
+            many_occurs_cols.append(i)
+    pos_col = random.choice(many_occurs_cols)
+    pos_row = random.choice(many_occurs_rows)
+    while field[pos_row][pos_col].strip():
+        pos_col = random.choice([0, 1, 2])
+        pos_row = random.choice([0, 1, 2])
+    return [pos_col, pos_row]
+
+
 def check_win_bot():
     if field[0][0] == field[1][0] == field[2][0] == 'O':
         return True
@@ -48,10 +74,10 @@ last_went = 1
 
 while True:
     if last_went == 1:
-        col = int(input('Колонка - '))-1
-        row = int(input('Ряд - '))-1
+        col = int(input('Колонка - ')) - 1
+        row = int(input('Ряд - ')) - 1
         while field[row][col].strip():
-            print('ВЫБРАННАЯ КЛЕТКА ЗАНЯТА!')
+            print('ВЫБРАННАЯ КЛЕТКА ЗАНЯТА!\nВведите новые координаты:')
             col = int(input('Колонка - ')) - 1
             row = int(input('Ряд - ')) - 1
         field[row][col] = 'X'
@@ -59,12 +85,8 @@ while True:
         print('Вы походили')
         display_field()
     else:
-        rand_col = random.choice([0, 1, 2])
-        rand_row = random.choice([0, 1, 2])
-        while field[rand_row][rand_col].strip():
-            rand_col = random.choice([0, 1, 2])
-            rand_row = random.choice([0, 1, 2])
-        field[rand_row][rand_col] = 'O'
+        chosen_pos = choose_pos()
+        field[chosen_pos[1]][chosen_pos[0]] = 'O'
         print('Соперник походил')
         display_field()
         last_went = 1
